185971b33a7cf7370a468763a92a70b3f3410b9b
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-07-15 09:57:01 +0300

    LS15/Restykized drag&drop menu

--- Lesson 15/code/js/script.js
+++ Lesson 15/code/js/script.js
@@ -323,7 +323,7 @@ document.addEventListener("click", menu);
 
 function menu(event) {
     if (event.target.closest('.menu__button')) {
-        menuBody.classList.toggle('_active');
+        menuBody.classList.toggle	('_active');
     }
     if (!event.target.closest('.menu')) {
         menuBody.classList.remove('_active');
