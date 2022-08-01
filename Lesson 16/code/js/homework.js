c07e2ca58e08698088e0e937881fe59992a2372b
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-01 11:42:11 +0300

    HW16/Added task for home work

--- Lesson 16/code/js/homework.js
+++ Lesson 16/code/js/homework.js
@@ -1,1 +1,7 @@
-// Урок 16. Домашняя работа. Home work.
+// lesson 16. Home work.
+/*
+1. Make a form and tested on her all events and metod form this lesson
+2. Add fill validation to text fields and symbols counter
+3. Prohibit the text insertion to textarea object this form
+
+*/
