558856cd4c0ac010ee63cda02cb36c1b96909590
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-01 12:30:37 +0300

    HW16/Edited the description to home work

--- Lesson 16/code/js/homework.js
+++ Lesson 16/code/js/homework.js
@@ -1,7 +1,7 @@
 // lesson 16. Home work.
 /*
-1. Make a form and tested on her all events and metod form this lesson
-2. Add fill validation to text fields and symbols counter
-3. Prohibit the text insertion to textarea object this form
-
+1. Create a form and work out all events and metods for this this lesson
+with it.
+2. Add validation of fiiled text fields and symbols counter.
+3. Prohibit the text insertion to textarea object this form.
 */
