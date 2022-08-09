8910d8e5d442a5528f2d90f70bc3f4adf3633209
S-Kozachuk <kozachuk-dev@yandex.ru>
2022-08-09 12:48:24 +0300

    HW16/Edited the reg. exp to user name

--- Lesson 16/Glo Academy/js/main.js
+++ Lesson 16/Glo Academy/js/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	const check = document.querySelector('.form-check-input');
 	let isSubmit = false;
 
-	const regExpName = /^[a-z0-9_-][A-z0-9]{3,16}$/;
+	const regExpName = /^[A-z0-9][a-z0-9_-]{3,16}$/;
 	const regExpEmail = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
 	const regExpPass = /^(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
 
